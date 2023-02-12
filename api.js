a43e271438b4a86400e226fb02a7ae0973ed7d0d
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2023-02-12 01:02:31 -0600

    API
    Configurando API

---
+++ api.js
@@ -0,0 +1,24 @@
+const bdcategoria = require('./dbcategoria');
+//Declarando las dependencias
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+var router = express.Router();
+//Para enviar POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/api', router);  //Ruta principal
+
+//Ruta general
+router.route('/categoria').get((request,response)=>{
+    bdcategoria.getCategoria().then(result => {
+        console.log(result);
+        response.json(result[0]);
+    })
+})
+
+var port = process.env.PORT || 8090; //Puerto para pruebas de entrega de paquetes 
+app.listen(port);  //Inicio del servicio
+console.log('Iniciando en el puerto: ' + port);
--- dbcategoria.js
+++ dbcategoria.js
@@ -1,9 +1,10 @@
-var config = require('./bdconfig'); //Instanciando las configuraciones}
+var config = require('./bdconfig'); //Instanciando las configuraciones
 const sql = require('mssql'); //Paquete mssql
 //Funcion asincrona que nos va a devolver informacion de la DB
 async function getCategoria(){
     try {
         let pool = await sql.connect(config);  //Espera la conexion con SQL
+        console.log('Conexion SQL exitosa!');
         let categorias = await pool.request.query("SELECT *FROM TM_CATEGORIA"); //Traemos la consulta
         return categorias.recordsets; //Retornamos la consulta
     } catch (error) {
